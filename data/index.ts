3185cbe8ba9bb228d244807d76d6c3bb9ed72ac2
syarizx-30 <241051004@students.unsurya.ac.id>
2026-05-24 18:36:37 +0700

    feat: Footer

--- data/index.ts
+++ data/index.ts
@@ -205,13 +205,16 @@ export const socialMedia = [
   {
     id: 1,
     img: "/git.svg",
+    link: "https://github.com/syaricz",
   },
   {
     id: 2,
     img: "/twit.svg",
+    link: "https://x.com/syaricz",
   },
   {
     id: 3,
     img: "/link.svg",
+    link: "https://www.linkedin.com/in/syamsurizal-undefined-21aa13405/",
   },
 ];
